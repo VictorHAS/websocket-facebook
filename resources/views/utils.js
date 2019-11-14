1600d255839e0d6638d0b91b41ab336e04cb83f0
Victor Hermes <victorhermesalves@gmail.com>
2019-11-14 11:11:01 -0300

    first commit

---
+++ resources/views/utils.js
@@ -0,0 +1,15 @@
+let online;
+
+function start() {
+  let ws = adonis.Ws('ws://localhost:3333');
+
+  ws = ws.connect();
+
+  ws.on('open', () => {
+    online = true;
+  });
+
+  ws.on('error', () => {
+    online = false;
+  });
+}
